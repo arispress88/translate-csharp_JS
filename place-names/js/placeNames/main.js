2c96cec86becdb493d3e5e2f697bbd5901365c49
Arnold Rispress <arnold.rispress@gmail.com>
2023-05-23 14:08:02 -0400

    completed place names exercise

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -5,8 +5,13 @@ const names = [
     "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
 ];
 
-console.log("All Pace Names");
+console.log("All Place Names");
 
 for (const name of names) {
   console.log(name);
 }
+
+//Solution
+console.log("\n'The' Place Names");
+const forTheNames = names.filter(n => n.startsWith("The"));
+forTheNames.forEach(n => console.log(n));
